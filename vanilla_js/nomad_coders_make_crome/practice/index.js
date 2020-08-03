c2ddca9650de02818c619e98642d281a2d30b915
jtbc <jtbc@naver.com>
2020-08-04 04:47:27 +0900

    feat : Add function when resize the web

--- vanilla_js/nomad_coders_make_crome/practice/index.js
+++ vanilla_js/nomad_coders_make_crome/practice/index.js
@@ -1,5 +1,7 @@
 const title = document.querySelector("#title");
-title.innerHTML = "Hello! From JS";
-title.style.color = "blue";
-console.dir(document);
-document.title = "I own you now";
+
+function handleResize(){
+  console.log("I have been resized");
+}
+
+window.addEventListener("resize", handleResize);
