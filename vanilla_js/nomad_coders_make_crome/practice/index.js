e1905af4c10909a12e0bb2fe4bdf021b01697e93
jtbc <jtbc@naver.com>
2020-08-04 02:33:56 +0900

    feat : Use backtick

--- vanilla_js/nomad_coders_make_crome/practice/index.js
+++ vanilla_js/nomad_coders_make_crome/practice/index.js
@@ -1,6 +1,6 @@
 function sayHello(name, age)
 {
-  console.log('Hello!', name, " you have ", age, "years of age.");
+  console.log(`Hello! ${name} you are ${age} years old.`);
 }
 
 sayHello("Nicolas", 15);
