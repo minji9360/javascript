e29e9898ff47562b82659078fd72926d25722cd3
jtbc <jtbc@naver.com>
2020-08-04 21:17:10 +0900

    Modify function responsed about mouseenter

--- vanilla_js/nomad_coders_make_crome/practice/index.js
+++ vanilla_js/nomad_coders_make_crome/practice/index.js
@@ -1,7 +1,19 @@
 const title = document.querySelector("#title");
 
-function handleClick(){
-  title.style.color = "black";
+const BASE_COLOR = "rgb(52, 73, 94)";
+const OTHER_COLOR = "#7f8c8d";
+
+function handleClick() {
+  const currentColor = title.style.color;
+  if (currentColor === BASE_COLOR) {
+    title.style.color = OTHER_COLOR;
+  } else {
+    title.style.color = BASE_COLOR;
+  }
 }
 
-title.addEventListener("click", handleClick);
+function init() {
+  title.style.color = BASE_COLOR;
+  title.addEventListener("mouseenter", handleClick);
+}
+init(); 
