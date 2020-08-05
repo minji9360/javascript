e4b26e278a402078ea97a977b8ac495354302e03
jtbc <jtbc@naver.com>
2020-08-06 06:11:55 +0900

    feat : Add file about getting weather

---
+++ vanilla_js/nomad_coders_make_crome/make_my_first_js_app/weather.js
@@ -0,0 +1,39 @@
+const API_KEY = "4a10f67fc9fd884cc994815bc506686f";
+const COORDS = "coords";
+
+function saveCoords(coordsObj){
+    localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSuccess(position){
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const coordsObj = {
+        latitude,
+        longitude
+    };
+    saveCoords(coordsObj);
+}
+
+function handleGeoError(){
+    console.log("Cant access geo location");
+}
+
+function askForCoords() {
+    navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
+}
+
+function loadCoords(){
+    const loadedCoords = localStorage.getItem(COORDS);
+    if(loadedCoords === null) {
+        askForCoords();
+    } else {
+
+    }
+}
+
+function init(){
+    loadCoords();
+}
+
+init();
