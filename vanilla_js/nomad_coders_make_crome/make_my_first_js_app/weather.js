917baa613afeeb346c3a0e09b74db649c8563a61
jtbc <jtbc@naver.com>
2020-08-06 22:51:20 +0900

    feat : Using weather API and Modify css

--- vanilla_js/nomad_coders_make_crome/make_my_first_js_app/weather.js
+++ vanilla_js/nomad_coders_make_crome/make_my_first_js_app/weather.js
@@ -1,5 +1,21 @@
+const weather = document.querySelector(".js-weather");
+
 const API_KEY = "4a10f67fc9fd884cc994815bc506686f";
 const COORDS = "coords";
+
+function getWeather(lat, lon){
+    fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+    )
+    .then(function(response){ 
+        return response.json();
+    })
+    .then(function(json) {
+        const temperature = json.main.temp;
+        const place = json.name;
+        weather.innerText = `${temperature} @ ${place}`;
+    });
+}
 
 function saveCoords(coordsObj){
     localStorage.setItem(COORDS, JSON.stringify(coordsObj));
@@ -13,6 +29,7 @@ function handleGeoSuccess(position){
         longitude
     };
     saveCoords(coordsObj);
+    getWeather(latitude, longitude);
 }
 
 function handleGeoError(){
@@ -28,7 +45,8 @@ function loadCoords(){
     if(loadedCoords === null) {
         askForCoords();
     } else {
-
+        const parsedCoords = JSON.parse(loadedCoords);
+        getWeather(parsedCoords.latitude, parsedCoords.longitude);
     }
 }
 
