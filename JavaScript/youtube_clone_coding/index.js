a15161539f53d65bcdf41616540984efa899bb1e
jtbc <jtbc@naver.com>
2020-08-14 18:31:56 +0900

    기능추가: 각 페이지마다 접속 시 로그 출력
    미들웨어 사용 위치에 따른 중요성을 배우기 위해 모든 페이지에 접속할
    때마다 로그를 출력하는 미들웨어를 생성

--- JavaScript/youtube_clone_coding/index.js
+++ JavaScript/youtube_clone_coding/index.js
@@ -9,6 +9,12 @@ const handleHome = (request, response) => response.send("Hello from 223");
 
 const handleProfile = (request, response) => response.send("You are on my profile");
 
+const betweenHome = (request, response, next) => {
+    console.log("I'm between");
+    next();
+}
+
+app.use(betweenHome);
 app.get("/", handleHome);
 app.get("/profile", handleProfile);
 app.listen(PORT, handleListening);
