f22f70b6a1457c54408e88022d2092a36407b0c0
jtbc <jtbc@naver.com>
2020-08-14 22:43:49 +0900

    기능추가: 다양한 middlewear 설치 및 설정 추가
    - logging을 위한 morgan
    - 보안을 위한 helmet
    - body로부터 정보를 얻을 수 있게 해주는 body-parser
    - cookie에 유저 정보 저장을 위해 사용할 cookie-parser

--- JavaScript/youtube_clone_coding/index.js
+++ JavaScript/youtube_clone_coding/index.js
@@ -1,4 +1,8 @@
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 const app = express();
 const PORT = 8080;
 
@@ -9,12 +13,11 @@ const handleHome = (request, response) => response.send("Hello from 223");
 
 const handleProfile = (request, response) => response.send("You are on my profile");
 
-const betweenHome = (request, response, next) => {
-    console.log("I'm between");
-    next();
-}
-
-app.use(betweenHome);
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("dev"));
 app.get("/", handleHome);
 app.get("/profile", handleProfile);
 app.listen(PORT, handleListening);
