8506f68226518005b83e8c42430e8d5f524e97c5
jtbc <jtbc@naver.com>
2020-08-15 18:53:55 +0900

    기능수정: URL에 따른 router 분리
    global, users, videos에 따른 라우터 모듈 생성

--- JavaScript/youtube_clone_coding/routers/globalRouter.js
+++ JavaScript/youtube_clone_coding/routers/globalRouter.js
@@ -1,5 +1,12 @@
 import express from "express";
+import routes from "../routes";
 
 const globalRouter = express.Router();
+
+globalRouter.get(routes.home, (request, response) => response.send("Home"));
+globalRouter.get(routes.join, (request, response) => response.send("Join"));
+globalRouter.get(routes.login, (request, response) => response.send("Login"));
+globalRouter.get(routes.logout, (request, response) => response.send("Logout"));
+globalRouter.get(routes.search, (request, response) => response.send("Search"));
 
 export default globalRouter;
