da14d70a299ecfcc1be6148689e0f395e39beff4
jtbc <jtbc@naver.com>
2020-08-17 21:45:01 +0900

    스타일: 줄바꿈 추가

--- JavaScript/youtube_clone_coding/routers/globalRouter.js
+++ JavaScript/youtube_clone_coding/routers/globalRouter.js
@@ -1,7 +1,14 @@
 import express from "express";
 import routes from "../routes";
-import { home, search } from "../controllers/videoController";
-import { join, login, logout } from "../controllers/userController";
+import { 
+    home, 
+    search 
+} from "../controllers/videoController";
+import { 
+    join, 
+    login, 
+    logout 
+} from "../controllers/userController";
 
 const globalRouter = express.Router();
 
