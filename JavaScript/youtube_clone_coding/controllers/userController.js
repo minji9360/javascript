a4fd62943f2441579e383de305f29eff7595d552
jtbc <jtbc@naver.com>
2020-08-17 20:53:55 +0900

    기능수정: controller 분리
    MVC 패턴으로 만들기 위해 videoRouter와 userRouter에서 각각의
    controller로 코드 분리

--- JavaScript/youtube_clone_coding/controllers/userController.js
+++ JavaScript/youtube_clone_coding/controllers/userController.js
@@ -1,3 +1,7 @@
 export const join = (request, response) => response.send("Join");
 export const login = (request, response) => response.send("Login");
 export const logout = (request, response) => response.send("Logout");
+export const users = (request, response) => response.send("Users");
+export const userDetail = (request, response) => response.send("User Detail");
+export const editProfile = (request, response) => response.send("Edit Profile");
+export const changePassword = (request, response) => response.send("Change Password");
--- JavaScript/youtube_clone_coding/controllers/videoController.js
+++ JavaScript/youtube_clone_coding/controllers/videoController.js
@@ -1,2 +1,7 @@
 export const home = (request, response) => response.send("Home");
 export const search = (request, response) => response.send("Search");
+export const videos = (request, response) => response.send("Videos");
+export const upload = (request, response) => response.send("Upload");
+export const videoDetail = (request, response) => response.send("Video Detail");
+export const editVideo = (request, response) => response.send("Edit Video");
+export const deleteVideo = (request, response) => response.send("Delete Video");
