669b6c72e40f98d3c639fb835b4a01164367b7d3
jtbc <jtbc@naver.com>
2020-08-19 13:04:43 +0900

    기능추가: 검색 시 해당 문자가 출력되는 기능 추가
    - 검색 기능을 위해 검색어를 받을 form 추가
    - 검색어를 search 페이지로 전달할 videoController 로직 추가

--- JavaScript/youtube_clone_coding/controllers/videoController.js
+++ JavaScript/youtube_clone_coding/controllers/videoController.js
@@ -1,14 +1,24 @@
 export const home = (request, response) => 
     response.render("home", { pageTitle: "Home" });
-export const search = (request, response) => 
-    response.render("search", { pageTitle: "Search" });
+
+export const search = (request, response) => {
+    const {
+        query: { term: searchingBy }
+    } = request;
+    response.render("search", { pageTitle: "Search", searchingBy });
+};
+
 export const videos = (request, response) => 
     response.render("videos", { pageTitle: "Videos" });
+
 export const upload = (request, response) => 
     response.render("upload", { pageTitle: "Upload" });
+
 export const videoDetail = (request, response) => 
     response.render("videoDetail", { pageTitle: "Video Detail" });
+
 export const editVideo = (request, response) => 
     response.render("editVideo", { pageTitle: "Edit Video" });
+
 export const deleteVideo = (request, response) => 
     response.render("deleteVideo", { pageTitle: "Delete Video" });
