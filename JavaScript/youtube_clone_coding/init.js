5c057197f7849072eb5da7e51da9232ce0969fa7
jtbc <jtbc@naver.com>
2020-08-21 22:59:36 +0900

    기능추가: dotenv 설정 추가

--- JavaScript/youtube_clone_coding/init.js
+++ JavaScript/youtube_clone_coding/init.js
@@ -1,7 +1,9 @@
 import "./db";
 import app from "./app";
+import dotenv from "dotenv";
+dotenv.config();
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 const handleListening = () => 
     console.log(`✅ Listening on: http://localhost:${PORT}`);
