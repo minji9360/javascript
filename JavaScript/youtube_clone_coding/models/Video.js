2f9997ce8970367adeb188df0a29da4b6d63eae4
jtbc <jtbc@naver.com>
2020-08-22 21:17:48 +0900

    기능추가: 코멘트 모델 생성
    mongoDB에 comment들의 형태를 정의하고 video와 연결할 파일 생성

--- JavaScript/youtube_clone_coding/models/Video.js
+++ JavaScript/youtube_clone_coding/models/Video.js
@@ -17,6 +17,10 @@ const VideoSchema = new mongoose.Schema({
     createdAt: {
         type: Date,
         default: Date.now
+    },
+    comments: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Comment"
     }
 });
 
