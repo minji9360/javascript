5e2cc6790f4cf1f407e56009794eff8213b190cc
jtbc <jtbc@naver.com>
2020-08-30 23:53:04 +0900

    기능추가: form, footer 등 scss 파일 추가
    - form, footer, socialLogin 디자인을 위한 scss 파일 추가
    - scss 저장 시 서버 재구동되지 않도록 package.json 수정했으나 실행되지
      않는 이슈 있음

--- JavaScript/youtube_clone_coding/middlewares.js
+++ JavaScript/youtube_clone_coding/middlewares.js
@@ -7,7 +7,7 @@ export const localsMiddleware = (request, response, next) => {
 	response.locals.siteName = "WeTube";
 	response.locals.routes = routes;
 	response.locals.user = {
-		isAuthenticated: true,
+		isAuthenticated: false,
 		id: 1,
 	};
 	next();
