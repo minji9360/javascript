e2a8ca6626e3411d19ba8c5d97bc2ddfd48f1583
jtbc <jtbc@naver.com>
2020-08-31 19:43:12 +0900

    기능추가: PassportJS 미들웨어 설치 및 회원가입 추가
    - 사용자 인증 구현을 위하여 passportJS 미들웨어 설치 및 설정
    - 회원가입 기능 추가

--- JavaScript/youtube_clone_coding/controllers/userController.js
+++ JavaScript/youtube_clone_coding/controllers/userController.js
@@ -1,20 +1,29 @@
 import routes from "../routes";
+import User from "../models/User";
 
 export const getJoin = (request, response) =>
 	response.render("join", { pageTitle: "Join" });
 
-export const postJoin = (request, response) => {
+export const postJoin = async (request, response) => {
 	const {
 		body: { name, email, password, password2 },
 	} = request;
 	if (password !== password2) {
 		response.status(400);
+		response.render("join", { pageTitle: "Join" });
 	} else {
-		// To Do: Register User
+		try {
+			const user = await User({
+				name,
+				email,
+			});
+			await User.register(user, password);
+		} catch (error) {
+			console.log(error);
+		}
 		// To Do: Log user in
 		response.redirect(routes.home);
 	}
-	response.render("join", { pageTitle: "Join" });
 };
 
 export const getLogin = (request, response) =>
