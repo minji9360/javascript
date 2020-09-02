f004b937a260fbea345d06e63bce1af963d58478
jtbc <jtbc@naver.com>
2020-09-02 10:41:49 +0900

    기능추가: 깃헙으로 회원가입/로그인 구현 완료
    - 깃헙 인증 후 정보를 가져오는 것  완료

--- JavaScript/youtube_clone_coding/controllers/userController.js
+++ JavaScript/youtube_clone_coding/controllers/userController.js
@@ -37,17 +37,31 @@ export const postLogin = passport.authenticate("local", {
 
 export const githubLogin = passport.authenticate("github");
 
-export const githubLoginCallback = (
-	accessTocken,
-	refreshToken,
-	profile,
-	cb
-) => {
-	console.log(accessTocken, refreshToken, profile, cb);
+export const githubLoginCallback = async (_, __, profile, cb) => {
+	const {
+		_json: { id, avatar_url, name, email },
+	} = profile;
+	try {
+		const user = await User.findOne({ email });
+		if (user) {
+			user.githubId = id;
+			user.save();
+			return cb(null, user);
+		}
+		const newUser = await User.create({
+			email,
+			name,
+			githubId: id,
+			avatarUrl: avatar_url,
+		});
+		return cb(null, newUser);
+	} catch (error) {
+		return cb(error);
+	}
 };
 
 export const postGithubLogIn = (request, response) => {
-	response.send(routes.home);
+	response.redirect(routes.home);
 };
 
 export const logout = (request, response) => {
