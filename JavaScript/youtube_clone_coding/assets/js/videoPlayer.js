9832598c397304c94896c622b1e97f2a02b05993
jtbc <jtbc@naver.com>
2020-09-06 18:44:04 +0900

    기능추가: 음소거 기능 구현
    - 비디오의 볼륨 버튼 클릭 시 음소거 기능을 사용할 수 있도록 구현
    - 음소거 시 적절한 아이콘으로 변경되도록 수정

--- JavaScript/youtube_clone_coding/assets/js/videoPlayer.js
+++ JavaScript/youtube_clone_coding/assets/js/videoPlayer.js
@@ -1,6 +1,7 @@
 const videoContainer = document.getElementById("jsVideoPlayer");
 const videoPlayer = document.querySelector("#jsVideoPlayer video");
 const playBtn = document.getElementById("jsPlayButton");
+const volumeBtn = document.getElementById("jsVolumeBtn");
 
 function handlePlayClick() {
 	if (videoPlayer.paused) {
@@ -11,9 +12,22 @@ function handlePlayClick() {
 		playBtn.innerHTML = '<i class="fas fa-play"></i>';
 	}
 }
+
+function handleVolumeClick() {
+	if (videoPlayer.muted) {
+		videoPlayer.muted = false;
+		volumeBtn.innerHTML = '<i class="fas fa-volume-up"></i>';
+	} else {
+		videoPlayer.muted = true;
+		volumeBtn.innerHTML = '<i class="fas fa-volume-mute"></i>';
+	}
+}
+
 function init() {
 	playBtn.addEventListener("click", handlePlayClick);
+	volumeBtn.addEventListener("click", handleVolumeClick);
 }
+
 if (videoContainer) {
 	init();
 }
